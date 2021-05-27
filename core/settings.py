7660fcceb9eb7af2092219b5b07dab5361f078a8
Savinay Kumar <iamsavinaykumar@gmail.com>
2021-05-27 10:31:09 +0530

    heroku deploy

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -141,3 +143,6 @@ LOGIN_REQUIRED_IGNORE_VIEW_NAMES = [                    # urls ignored by the lo
     'logout',
     'about',
 ]
+
+
+django_heroku.settings(locals())
